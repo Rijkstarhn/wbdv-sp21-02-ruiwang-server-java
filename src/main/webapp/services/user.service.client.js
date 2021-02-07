51c33970ab2bf15ec46266c44db3c585e5b98ed6
Rijkstarhn <32129291+Rijkstarhn@users.noreply.github.com>
2021-02-07 18:01:24 +0800

    Added create, delete feature;

---
+++ src/main/webapp/services/user.service.client.js
@@ -0,0 +1,40 @@
+let adminUserServiceClient = new AdminUserServiceClient();
+
+function AdminUserServiceClient() {
+    this.createUser = createUser;
+    this.findAllUsers = findAllUsers;
+    this.findUserById = findUserById;
+    this.deleteUser = deleteUser;
+    this.updateUser = updateUser;
+    this.url = 'https://wbdv-generic-server.herokuapp.com/api/ray1991/users';
+    let self = this;
+
+    function createUser(user) {
+        return fetch(self.url, {
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {'content-type': 'application/json'}
+        }).then(function (response) {
+                return response.json()
+            }
+        )
+    }
+
+    function findAllUsers() {
+        return fetch(self.url).then(function (response) {
+            return response.json()
+        })
+    }
+
+    function findUserById(userId) {
+    }
+
+    function updateUser(userId, user) {
+    }
+
+    function deleteUser(userId) {
+        return fetch(`${self.url}/${userId}`, {method: 'delete'}).then(function (response) {
+            return response.json()
+        })
+    }
+}
--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -1,33 +1,16 @@
-let courses = [
-    // {userName: 'wr', password: 1234, firstName: 'Rui', lastName: 'Wang', role: 'Student'},
-    // {userName: 'wr', password: 1234, firstName: 'Rui', lastName: 'Wang', role: 'Student'}
-]
+let users = []
 
-$(".wbdv-create-btn").click(function () {
-
-    let newCourse = {
-        userName: `${$('.usernameFld').val()}`,
-        password: '******',
-        firstName: `${$('.firstNameFld').val()}`,
-        lastName: `${$('.lastNameFld').val()}`,
-        role: `${$('.roleFld').val()}`,
-    }
-    courses.push(newCourse)
-    renderCourse(courses)
-})
-
-
-function renderCourse(courses) {
+function renderUser(users) {
     let tbody = $('.wbdv-tbody');
     tbody.empty()
-    for (let i = 0; i < courses.length; i++) {
+    for (let i = 0; i < users.length; i++) {
         tbody.prepend(`
             <tr class="wbdv-template wbdv-user wbdv-hidden">
-                <td class="wbdv-username">${courses[i].userName}</td>
-                <td>&nbsp;</td>
-                <td class="wbdv-first-name">${courses[i].firstName}</td>
-                <td class="wbdv-last-name">${courses[i].lastName}</td>
-                <td class="wbdv-role">${courses[i].role}</td>
+                <td class="wbdv-username">${users[i].userName}</td>
+                <td>&nbsp;</td> // don't show pwd for security reason
+                <td class="wbdv-first-name">${users[i].firstName}</td>
+                <td class="wbdv-last-name">${users[i].lastName}</td>
+                <td class="wbdv-role">${users[i].role}</td>
                 <td class="wbdv-actions">
             <span class="pull-right">
               <button class="btn wbdv-delete-btn" id="${i}"><i class="fa-2x fa fa-times wbdv-remove"></i></button>
@@ -37,16 +20,43 @@ function renderCourse(courses) {
             </tr>
         `)
     }
-    $('.wbdv-delete-btn').click(function (event) {
-        let selectedButton = $(event.currentTarget)
-        let selectedButtonId = selectedButton.attr("id")
-        console.log(selectedButtonId)
-        console.log(courses)
-        courses.splice(selectedButtonId, 1)
-        console.log(courses)
-        renderCourse(courses)
+    $('.wbdv-delete-btn').click(deleteUser)
+}
+
+function createUser() {
+    let newUser = {
+        userName: `${$('.usernameFld').val()}`,
+        password: `${$('.passwordFld').val()}`,
+        firstName: `${$('.firstNameFld').val()}`,
+        lastName: `${$('.lastNameFld').val()}`,
+        role: `${$('.roleFld').val()}`,
+    }
+    users.push(newUser)
+    adminUserServiceClient.createUser(newUser).then(function (response) {
+        renderUser(users)
     })
 }
 
-renderCourse(courses)
+function deleteUser(event) {
+    let selectedButton = $(event.currentTarget)
+    let selectedButtonIndex = selectedButton.attr("id")
+    let selectedButtonId = users[selectedButtonIndex]._id
+
+    adminUserServiceClient.deleteUser(selectedButtonId).then(function (res) {
+        users.splice(selectedButtonIndex, 1)
+        renderUser(users)
+    })
+}
+
+function init() {
+    let $createBtn = $(".wbdv-create-btn")
+    $createBtn.click(createUser)
+    adminUserServiceClient.findAllUsers().then(function (actualUsers) {
+        users = actualUsers
+        renderUser(users)
+    })
+}
+
+$(init)
+
 
