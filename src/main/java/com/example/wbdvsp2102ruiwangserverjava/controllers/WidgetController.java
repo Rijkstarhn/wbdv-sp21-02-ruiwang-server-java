506a1979dfb0b49f84b2068982b785ce6ccc29c9
Rijkstarhn <32129291+Rijkstarhn@users.noreply.github.com>
2021-03-17 14:37:26 -0700

    Created Widget, WidgetController and WidgetService.

---
+++ src/main/java/com/example/wbdvsp2102ruiwangserverjava/controllers/WidgetController.java
@@ -0,0 +1,40 @@
+package com.example.wbdvsp2102ruiwangserverjava.controllers;
+
+import com.example.wbdvsp2102ruiwangserverjava.models.Widget;
+import com.example.wbdvsp2102ruiwangserverjava.services.WidgetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class WidgetController {
+
+  @Autowired WidgetService service;
+
+  @GetMapping("/api/widgets")
+  public List<Widget> findAllWidgets() {
+    return service.findAllWidgets();
+  }
+
+  @GetMapping("/api/topics/{tid}/widgets")
+  public List<Widget> findWidgetsForTopic(@PathVariable("tid") String topicId) {
+    return service.findWidgetsForTopic(topicId);
+  }
+
+  @PostMapping("/api/topics/{tid}/widgets")
+  public Widget createWidget(@PathVariable("tid") String tid, Widget widget) {
+    return service.createWidget(tid, widget);
+  }
+
+  @PutMapping("/api/widgets/{wid}")
+  public int updateWidget(@PathVariable("wid") String wid, Widget widget) {
+    return service.updateWidget(wid, widget);
+  }
+
+  @DeleteMapping("/api/widgets/{wid}")
+  public int deleteWidget(@PathVariable("wid") String wid) {
+    return service.deleteWidget(wid);
+  }
+}
---
+++ src/main/java/com/example/wbdvsp2102ruiwangserverjava/services/WidgetService.java
@@ -0,0 +1,68 @@
+package com.example.wbdvsp2102ruiwangserverjava.services;
+
+import com.example.wbdvsp2102ruiwangserverjava.models.Widget;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class WidgetService {
+  private List<Widget> widgets = new ArrayList<Widget>();
+
+  {
+    Widget w1 = new Widget(123l, "ABC123", "HEADING", 1, "Widgets for Topic ABC123", "123");
+    Widget w2 = new Widget(234l, "ABC123", "PARAGRAPH", 1, "Lorem Ipsum", "234");
+    Widget w3 = new Widget(345l, "ABC234", "HEADING", 2, "Widgets for Topic ABC234", "345");
+    Widget w4 = new Widget(456l, "ABC234", "PARAGRAPH", 1, "Lorem Ipsum", "456");
+    Widget w5 = new Widget(567l, "ABC234", "PARAGRAPH", 1, "Lorem Ipsum", "567");
+
+    widgets.add(w1);
+    widgets.add(w2);
+    widgets.add(w3);
+    widgets.add(w4);
+    widgets.add(w5);
+  }
+
+  public List<Widget> findAllWidgets() {
+    return widgets;
+  }
+
+  public Widget createWidget(String tid, Widget widget) {
+    Long id = new Date().getTime();
+    Widget addWidget = new Widget(id, tid, "HEADING", 1, "LOk Tar!", Long.toString(id));
+    widgets.add(addWidget);
+    return addWidget;
+  }
+
+  public List<Widget> findWidgetsForTopic(String topicId) {
+    List<Widget> ws = new ArrayList<Widget>();
+    for (Widget widget : widgets) {
+      if (topicId.equals(widget.getTopicId())) {
+        ws.add(widget);
+      }
+    }
+    return ws;
+  }
+
+  public int updateWidget(String wid, Widget widget) {
+    for (Widget updateWidget : widgets) {
+      if (updateWidget.getWidgetId().equals(wid)) {
+        updateWidget = widget;
+        return 1;
+      }
+    }
+    return 0;
+  }
+
+  public int deleteWidget(String wid) {
+    for (Widget widget : widgets) {
+      if (widget.getWidgetId().equals(wid)) {
+        widgets.remove(widgets.indexOf(widget));
+        return 1;
+      }
+    }
+    return 0;
+  }
+}
